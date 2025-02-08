be626a7ba6e399aacf460672102984002a5ec130
youngmin109 <qhqo3325@gmail.com>
2025-02-08 15:32:40 +0900

    update

---
+++ php/1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+    <h1>String & String Operator</h1>
+    <?php
+    echo 3 + 3
+    ?>
+
+    <h2>concatenation opertator</h2>
+    <?php
+    echo "hello "."world"; # .은 문자열 + 문자열
+    ?>
+</body>
+
+</html>
